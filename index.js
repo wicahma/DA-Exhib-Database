70b65f574e45531266bc6a178b72f6e245db0b4b
firoos18 <itsfiroos@gmail.com>
2022-11-27 18:43:45 +0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,5 @@
+var mongoose = require("mongoose");
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://firoos:firoos18@cluster0.aiwbnlx.mongodb.net/?retryWrites=true&w=majority";
