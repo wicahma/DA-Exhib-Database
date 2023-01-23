07088a83d6cc9fa1ffa835b3f3e1ee46e8ccacfb
Teguh Dwi Cahya Kusuma <teguh.dc.kusuma@gmail.com>
2023-01-24 02:56:35 +0700

    bug: deleting fckn slash bruh

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://da-exhib.vercel.app/",
+    origin: "https://da-exhib.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
